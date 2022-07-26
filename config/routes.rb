94ee6a79bb4566553fc8faec6cae5baad7fed38e
masashi007-t <tomims.mrag2@gmail.com>
2022-07-27 04:13:08 +0900

    四回目

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
   get 'top' => 'homes#top'
+  
+  get 'posts' => 'posts#index'
+  get 'posts/new' => "posts#new"
+  post 'posts' => 'posts#create'
+  get "/posts/:id" => "posts#show"
+  get 'posts/:id/edit' => 'posts#edit'
+  patch 'posts/:id' => 'posts#update'
+  delete "/posts/:id" => "posts#destroy"
 end
